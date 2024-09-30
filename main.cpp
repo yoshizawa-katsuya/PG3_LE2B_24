15783f4c7af1a1fb14057d4c20d8361eab69c271
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-09-30 12:56:47 +0900

    再帰関数を作成

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include "math.h"
 
+int Recursive(int n) {
+	if (n <= 1) {
+		return 100;
+	}
+
+	return (Recursive(n - 1) * 2 - 50);
+}
 
 int main() {
 
+	int hour = 9;
+	
+	int normal = 1072 * hour;
 
-	printf("%d\n", Min<int>(93, 128));
-	printf("%f\n", Min<float>(78.5f, 12.1f));
-	printf("%lf\n", Min<double>(34.8, 67.9));
-	printf("%c\n", Min<char>('a', 't'));
+	int recorsive = Recursive(hour);
+
+	printf("一般的な賃金体系 %d\n", normal);
+
+	printf("再帰的な賃金体系 %d\n", recorsive);
+
+	if (normal > recorsive) {
+		printf("一般的な賃金体系の方が儲かる");
+	}
+	else if (recorsive > normal) {
+		printf("再帰的な賃金体系の方が儲かる");
+	}
 
 	return 0;
 }
