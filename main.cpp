7849695daa00bff450416389b8d365d89553cd52
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-10-16 10:47:39 +0900

    ラムダ式を使用

--- main.cpp
+++ main.cpp
@@ -2,59 +2,59 @@
 #include "math.h"
 #include <random>
 #include <windows.h>
+#include <functional>
 
-typedef void (*PFunc)(int, int);
+void SetTimeout(std::function<void()> func, int delay) {
 
-void Dice(int num, int dice) {
+	Sleep(delay);
 
-	
-
-	if (num == 0) {
-		if (dice % 2 == 0) {
-			printf("サイコロ %d 正解\n", dice);
-		}
-		else {
-			printf("サイコロ %d 不正解\n", dice);
-		}
-	}
-	else if (num == 1) {
-		if (dice % 2 == 0) {
-			printf("サイコロ %d 不正解\n", dice);
-		}
-		else {
-			printf("サイコロ %d 正解\n", dice);
-		}
-	}
-
-	return;
-}
-
-void Input(PFunc p) {
-
-	printf("偶数なら0、奇数なら1を入力\n");
-
-	int num;
-
-	scanf_s("%d", &num);
-
-	std::random_device rnd;
-	std::mt19937 mt(rnd());
-	std::uniform_int_distribution<> distribution(1, 6);
-
-	int dice = distribution(mt);
-
-	Sleep(3000);
-
-	p(num, dice);
+	func();
 
 }
 
 int main() {
 
-	PFunc p;
-	p = Dice;
+	int num;
+	int dice;
 
-	Input(p);
+	std::function<void()> Dice = [&]() {
+
+		if (num == 0) {
+			if (dice % 2 == 0) {
+				printf("サイコロ %d 正解\n", dice);
+			}
+			else {
+				printf("サイコロ %d 不正解\n", dice);
+			}
+		}
+		else if (num == 1) {
+			if (dice % 2 == 0) {
+				printf("サイコロ %d 不正解\n", dice);
+			}
+			else {
+				printf("サイコロ %d 正解\n", dice);
+			}
+		}
+
+		return;
+	};
+
+	std::function<void()> Input = [&]() {
+
+		printf("偶数なら0、奇数なら1を入力\n");
+
+		scanf_s("%d", &num);
+
+		std::random_device rnd;
+		std::mt19937 mt(rnd());
+		std::uniform_int_distribution<> distribution(1, 6);
+
+		dice = distribution(mt);
+
+		SetTimeout(Dice, 3000);
+	};
+
+	Input();
 
 	return 0;
 }
